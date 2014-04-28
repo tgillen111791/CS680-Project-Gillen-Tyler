bddacdff466a328482bc6fb462dda8504d6f1d23
Tyler Gillen <tgillen111791@gmail.com>
2014-04-28 16:31:33 -0500

    Add Items to the To-Do List

--- personalAssistant.py
+++ personalAssistant.py
@@ -1,9 +1,32 @@
 import time
 import datetime
+import cPickle
 import os
 from sys import exit
 
 def main():
+    print '''
+    Personal Assistant
+    
+    1: To-Do List
+    2: Birthdays
+    3: Exit 
+    '''
+    answer = raw_input('\nEnter > ')
+    answer = answer.lower()
+    if answer == '1':
+        toDoMain()
+        main()
+    elif answer == '2':
+        main()
+    elif answer == '3':
+        print 'Goodbye!'
+        exit()
+    else:
+        print '\nPlease select an option!'
+        main()
+        
+def toDoMain():
     print '\nYour current Todo list is: \n'
     if os.path.exists('todo.dat'):
         try:
@@ -15,40 +38,94 @@ def main():
                 print k, v[0], v[1]
         finally:    
             fname.close()
-            menu()
+            toDoMenu()
     else:
         todo = {}
-        menu()
-
-def menu():
+        toDoMenu()
+        
+def toDoMenu():
     print '''
-    Todo List
+    To-Do List
     
-    Enter I: Show Current List
-    Enter A: Add Todo
-    Enter D: Remove Todo
-    Enter E: Edit Todo
-    Enter X: Exit 
+    1: Show Current List
+    2: Add To-Do Items
+    3: Remove To-Do Items
+    4: Edit To-Do Items
+    5: Exit 
     '''
     answer = raw_input('\nEnter > ')
     answer = answer.lower()
-    if answer == 'a':
-        add_todo()
+    if answer == '1':
+        toDoMenu()
+    elif answer == '2':
+    	addToDo()
+        toDoMain()
+    elif answer == '3':
+        deleteToDo()
+        toDoMenu()
+    elif answer == '4':
+        editToDo()
+        toDoMenu()
+    elif answer == '5':
         main()
-    elif answer == 'i':
-        main()
-    elif answer == 'd':
-        del_todo()
-        main()
-    elif answer == 'e':
-        edit_todo()
-        main()
-    elif answer == 'x':
-        print 'Goodbye'
-        exit()
     else:
-        print '\nPlease enter an option!'
-        main()
+        print '\nPlease select an option!'
+        toDoMain()
+
+def addToDo():
+    if os.path.exists('todo.dat'):
+        try:
+            fname = open('todo.dat', 'rb')
+            data = cPickle.Unpickler(fname)
+            todo = data.load()
+            saveToDo(todo)
+        finally:    
+            fname.close()
+    else:
+        todo = {}
+    
+    try:
+        key = raw_input('Enter To-Do Item: ')
+        print '\n', key, 'has been added.'
+        print 'Enter a due date for this item: '
+        curr_date = time.strftime('%Y %m %d', time.gmtime())
+        print 'Format as ', curr_date
+        yr = getInteger(raw_input,'\nEnter Year: ')
+        mt = getInteger(raw_input,'Enter Month: ')
+        dy = getInteger(raw_input, 'Enter Day: ')
+        hr = getInteger(raw_input,'Enter Hour (0-23): ')
+        mn = getInteger(raw_input,'Enter Minute (01-59): ')
+        sec = 0
+        datevalue = datetime.date(yr, mt, dy)
+        hourvalue = datetime.time(hr, mn, sec)
+        todo[key] = datevalue, hourvalue
+        saveToDo(todo)
+        print '\nYour updated To-Do list is: \n'
+        for k, v in todo.iteritems():
+            print k, datevalue, hourvalue
+        response = raw_input('\nDo you want to add another To-Do item? (y/n) ')
+        if response == 'y':
+            addToDo()
+        else:
+            print 'Goodbye'
+    except KeyError, e:
+        print '\nError! Please enter the To-Do item you want to add.\n'
+
+def saveToDo(todo):
+    fname = open('todo.dat', 'w')
+    object = cPickle.Pickler(fname)
+    object.dump(todo)
+    fname.close()
+    
+def getInteger(retrieve,question,attempts=3):
+    while attempts > 0:
+        num = retrieve(question)
+        try: 
+            return int(num)
+        except ValueError:
+            print "Opps, You must enter a number!"
+        attempts -= 1
+    raise BadUserError("Too many incorrect attempts!")
 
 if __name__ == '__main__':
     main()
